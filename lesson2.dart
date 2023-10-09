bff6517409751c07faf35cfcbca0c8cbe1b785fa
bbaana <kurmankozoevabaana@gmail.com>
2023-10-09 18:27:40 +0600

    first commit

---
+++ lesson2.dart
@@ -0,0 +1,79 @@
+
+
+void main(List<String> args) {
+  int a = 100;
+  int b = 20;
+  print(a + b);
+  int c = a + b;
+  print(c);
+  int d = a - b;
+  print(d);
+  int e = a * b;
+  print(e);
+
+  int f = a ~/ b;
+  print(f);
+
+  double a1 = 5.5;
+  double b1 = 2.2;
+  double c1 = a1 / b1;
+  print(c1);
+
+  int a2=20;
+  int b2=20;
+  print(a2>b2);
+  print(a2<b2);
+  print(a2>=b2);
+   print(a2<=b2);
+print(a2==b2);
+print(a2 !=b2);
+
+
+
+
+
+  
+Map userInfo = {
+    "name": "baiana",
+    "surname": "baktybekova",
+    "age": 77,
+    "isMan": true,
+  };
+  print(userInfo);
+print(userInfo["age"]);
+
+var test = 10;
+final userName;
+userName = 'bayana';
+
+const userName2 = 'bayana'; 
+
+
+
+
+print(test);
+print(test.runtimeType);
+
+test = 20;
+test = 30;
+
+  dynamic test2 = 11;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
---
+++ lesson3.dart
@@ -0,0 +1,86 @@
+void main(List<String> args) {
+  Map userInfoA = {
+    "name": "asylbek",
+    "surname": "tynybekovv",
+    "age": 22,
+    "isMan": true,
+  };
+  Map userInfoB = {
+    'name': "medina",
+    'surname': 'statbekova',
+    'age': 19,
+    'isMan': false,
+  };
+  Map userInfoC = {
+    'name': "Aizirek",
+    "surname": "jyrgalbekova",
+    "age": 20,
+    "isMan": false,
+  };
+  Map userInfoD = {
+    'name': "Munara",
+    "surname": "Satybaldyeva",
+    "age": 53,
+    "isMan": false,
+  };
+  Map userInfoF = {
+    'name': "Ulan",
+    "surname": "Kurmankojoev",
+    "age": 26,
+    "isMan": true,
+  };
+  print(userInfoA);
+  print(userInfoA["age"]);
+
+  print(userInfoB);
+  print(userInfoB["age"]);
+
+  print(userInfoC);
+  print(userInfoC["age"]);
+
+  print(userInfoD);
+  print(userInfoD["age"]);
+
+  print(userInfoF);
+  print(userInfoF["age"]);
+
+  var test = 22;
+  final userName;
+  userName = 'Asylbek';
+
+  var test1 = 19;
+  final userName1;
+  userName1 = 'Medina';
+
+  var test3 = 20;
+  final userName3;
+  userName3 = 'Aizirek';
+
+  var test4 = 53;
+  final userName4;
+  userName4 = 'Munara';
+
+  var test5 = 26;
+  final userName5;
+  userName5 = 'Ulan';
+
+  print(test);
+  print(test);
+  print(test);
+  print(test);
+  print(test);
+
+  const userName6 = 'Asylbek';
+  const userName7 = 'Medina';
+  const userName8 = 'Aizirek';
+  const userName9 = 'Munara';
+  const userName10 = 'Ulan';
+
+  dynamic testA = true;
+  dynamic testB = false;
+  dynamic testC = false;
+  dynamic testD = false;
+  dynamic testF = true;
+}
+
+
